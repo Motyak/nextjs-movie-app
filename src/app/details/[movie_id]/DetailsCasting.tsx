4f47923d314fbeaed2db9b7c8f1f5af4bd98d90b
Motyak <m0tyak_@live.fr>
2025-12-30 17:53:37 +0100

    fix casting photos

--- src/app/details/[movie_id]/DetailsCasting.tsx
+++ src/app/details/[movie_id]/DetailsCasting.tsx
@@ -13,14 +13,17 @@ export default function DetailsCasting({movie_id}: {movie_id: number}) {
             <h2 className={`${inter400.className} text-xl`}>Casting</h2>
             <div className="flex flex-wrap gap-7 pt-4">
                 {movieInfo?.casting.filter(cast => cast.photo !== null).map(cast => (
-                    <div key={cast.name}>
-                        <Image
-                            src={`/api/image/w185/${cast.photo}`}
-                            width={160}
-                            height={160}
-                            alt="photo"
-                            loading="eager" // fix warning
-                        />
+                    <div key={cast.name} style={{width: "160px"}}>
+                        <div className="relative" style={{height: "160px"}}>
+                            <Image
+                                src={`/api/image/w185/${cast.photo}`}
+                                layout="fill"
+                                objectFit="cover"
+                                style={{objectPosition: "center"}}
+                                alt="photo"
+                                loading="eager" // fix warning
+                            />
+                        </div>
                         <p className={`${inter400.className} text-lg pt-3`}>{cast.name}</p>
                         <p className={`${archivo400.className} text-sm`} style={{color: "#9B9B9B"}}>{cast.character}</p>
                     </div>
