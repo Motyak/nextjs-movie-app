74f3026601c2e99d3038604bcf8e717484d87fa2
Motyak <m0tyak_@live.fr>
2025-12-31 00:33:30 +0100

    carousel arrows OK

--- src/app/HorizontalCarousel.tsx
+++ src/app/HorizontalCarousel.tsx
@@ -19,7 +19,12 @@ const PrevArrow = (props: any) => {
 const NextArrow = (props: any) => {
     let { className, style, onClick } = props
     return (
-        <p className={className} style={style} onClick={onClick}>B</p>
+        <div onClick={onClick} className={`${className} relative`} style={{...style, width: "64px", height: "40px", borderRadius: "48px", backgroundColor: "rgb(16, 16, 16, 0.8)"}}>
+            <svg viewBox="0 0 24 24" fill="#FFFFFF" strokeWidth="0.8" stroke="currentColor"
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <path d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z" />
+            </svg>
+        </div>
     )
 }
 
