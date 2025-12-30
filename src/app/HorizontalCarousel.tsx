7ef821bc98f2729293543266131aac8175fc94bc
Motyak <m0tyak_@live.fr>
2025-12-31 00:22:00 +0100

    horizontal carousel arrows WIP

--- src/app/HorizontalCarousel.tsx
+++ src/app/HorizontalCarousel.tsx
@@ -2,8 +2,26 @@
 
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
 import "@/app/HorizontalCarousel.css"
+
+const PrevArrow = (props: any) => {
+    let { className, style, onClick } = props
+    return (
+        <div onClick={onClick} className={`${className} relative`} style={{...style, width: "64px", height: "40px", borderRadius: "48px", backgroundColor: "rgb(16, 16, 16, 0.8)"}}>
+            <svg viewBox="0 0 24 24" fill="#FFFFFF" strokeWidth="0.8" stroke="currentColor"
+                className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <path d="M11.7071 4.29289C12.0976 4.68342 12.0976 5.31658 11.7071 5.70711L6.41421 11H20C20.5523 11 21 11.4477 21 12C21 12.5523 20.5523 13 20 13H6.41421L11.7071 18.2929C12.0976 18.6834 12.0976 19.3166 11.7071 19.7071C11.3166 20.0976 10.6834 20.0976 10.2929 19.7071L3.29289 12.7071C3.10536 12.5196 3 12.2652 3 12C3 11.7348 3.10536 11.4804 3.29289 11.2929L10.2929 4.29289C10.6834 3.90237 11.3166 3.90237 11.7071 4.29289Z" />
+            </svg>
+        </div>
+    )
+}
+
+const NextArrow = (props: any) => {
+    let { className, style, onClick } = props
+    return (
+        <p className={className} style={style} onClick={onClick}>B</p>
+    )
+}
 
 export default function HorizontalCarousel({children}: Readonly<{children: React.ReactNode}>) {
     const settings = {
@@ -11,6 +29,9 @@ export default function HorizontalCarousel({children}: Readonly<{children: React
         // slidesToShow: 7.5,
         slidesToScroll: 5,
         variableWidth: true,
+        draggable: false, // only for desktop
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />,
     }
     return (
         <div className="slider-container">
--- src/app/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function HomeLoading() {
-    return <div className="border border-red-500 border-dotted">Loading...</div>
-}
