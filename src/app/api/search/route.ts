0961a92341333e9699513867c127223fce918f01
Motyak <m0tyak_@live.fr>
2026-01-05 05:05:11 +0100

    /search for person OK

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -10,26 +10,74 @@ const parseReleaseYear = (x: string | number | null): number | undefined => {
 
 export async function GET(_req: Request) {
     let query = new URL(_req.url).searchParams.get("q") ?? ""
-    let url = "https://api.themoviedb.org/3/search/movie"
-        + `?query=${encodeURIComponent(query)}`
-        + "&language=fr"
-        + "&region=FR"
+    let searchMovies: Promise<number[]>
+    let searchMulti: Promise<any>
 
-    let req = fetch(url, {
-        headers: {
-            "Authorization": `Bearer ${getBearerToken()}`,
-        },
+    /* /movie query */
+    {
+        let url = "https://api.themoviedb.org/3/search/movie"
+            + `?query=${encodeURIComponent(query)}`
+            + "&language=fr"
+            + "&region=FR"
+
+        let req = fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${getBearerToken()}`,
+            },
+        })
+
+        searchMovies = req.then(res => res.json()).then(obj => {
+            let movieIds: number[] = obj.results.slice(0, 10)
+                .map((x: any) => {
+                    let releaseYear = parseReleaseYear(x.release_date)
+                    let movie = releaseYear === undefined ? x.title : `${x.title} (${releaseYear})`
+                    return {movie, movieId: x.id}
+                })
+            return movieIds
+        })
+    }
+
+    /* /multi query */
+    {
+        let url = "https://api.themoviedb.org/3/search/multi"
+            + `?query=${encodeURIComponent(query)}`
+            + "&language=fr"
+
+        let req = fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${getBearerToken()}`,
+            },
+        })
+
+        searchMulti = req.then(res => res.json())
+    }
+
+    return await searchMulti.then(async obj => {
+        if (obj.results.length === 0 || obj.results[0].media_type !== "person") {
+            let response = await searchMovies
+            return new Response(JSON.stringify(response))
+        }
+        
+        /* /movie_credits request */
+        let personId = obj.results[0].id
+        let url = `https://api.themoviedb.org/3/person/${personId}/movie_credits?language=fr`
+
+        let req = fetch(url, {
+            headers: {
+                "Authorization": `Bearer ${getBearerToken()}`,
+            },
+        })
+
+        let response = await req.then(res => res.json()).then(obj => {
+            let movieIds: number[] = obj.cast.slice(0, 10)
+                .map((x: any) => {
+                    let releaseYear = parseReleaseYear(x.release_date)
+                    let movie = releaseYear === undefined ? x.title : `${x.title} (${releaseYear})`
+                    return {movie, movieId: x.id}
+                })
+            return movieIds
+        })
+
+        return new Response(JSON.stringify(response))
     })
-
-    let response = await req.then(res => res.json()).then(obj => {
-        let movieIds: number[] = obj.results.slice(0, 10)
-            .map((x: any) => {
-                let releaseYear = parseReleaseYear(x.release_date)
-                let movie = releaseYear === undefined ? x.title : `${x.title} (${releaseYear})`
-                return {movie, movieId: x.id}
-            })
-        return movieIds
-    })
-
-    return new Response(JSON.stringify(response))
 }
